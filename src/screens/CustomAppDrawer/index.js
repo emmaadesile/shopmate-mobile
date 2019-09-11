b616103d584ff069177c51ca41ec26545fddb737
Emmanuel Adesile <emma2adesile@gmail.com>
2019-09-11 16:45:57 +0100

    ft(handle-logout): users should be able to logout

--- src/screens/CustomAppDrawer/index.js
+++ src/screens/CustomAppDrawer/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { View, Image, TouchableOpacity } from "react-native";
+import { getUserToken, removeUserToken } from "../../store/actions/authAction";
 
 import {
   Container,
@@ -18,72 +19,102 @@ import Bag from "../../components/Bag";
 import Stores2 from "../../components/Stores2";
 import Polygon from "../../components/Polygon";
 
-const CustomAppDrawer = ({ navigation: { navigate } }) => {
-  return (
-    <Container>
-      <TopSection>
-        <View style={{ flexDirection: "row" }}>
-          <IconWrapper>
-            <TouchableOpacity
-              onPress={() => navigate("Shop")}
-              style={{ alignItems: "center" }}
-            >
-              <Polygon />
-              <IconName>Shop</IconName>
-            </TouchableOpacity>
-          </IconWrapper>
-          <IconWrapper>
-            <TouchableOpacity
-              onPress={() => navigate("Bag")}
-              style={{ alignItems: "center" }}
-            >
-              <Bag strokeColor="#fff" fillColor="#fff" />
-              <IconName>Bag</IconName>
-            </TouchableOpacity>
-          </IconWrapper>
-        </View>
+class CustomAppDrawer extends React.Component {
+  state = {
+    isLoggeIn: false
+  };
+  async componentDidMount() {
+    const userToken = await getUserToken();
 
-        <View style={{ flexDirection: "row" }}>
-          <IconWrapper>
-            <TouchableOpacity
-              onPress={() => navigate("Inspiration")}
-              style={{ alignItems: "center" }}
-            >
-              <Image source={require("../../../assets/sun.png")} />
-              <IconName>Inspiration</IconName>
+    if (userToken) {
+      this.setState({ isLoggeIn: true });
+    }
+  }
+
+  handleLogout = async () => {
+    await removeUserToken();
+    this.props.navigation.navigate("Login");
+  };
+
+  render() {
+    const { navigate } = this.props.navigation;
+
+    return (
+      <Container>
+        <TopSection>
+          <View style={{ flexDirection: "row" }}>
+            <IconWrapper>
+              <TouchableOpacity
+                onPress={() => navigate("Shop")}
+                style={{ alignItems: "center" }}
+              >
+                <Polygon />
+                <IconName>Shop</IconName>
+              </TouchableOpacity>
+            </IconWrapper>
+            <IconWrapper>
+              <TouchableOpacity
+                onPress={() => navigate("Bag")}
+                style={{ alignItems: "center" }}
+              >
+                <Bag strokeColor="#fff" fillColor="#fff" />
+                <IconName>Bag</IconName>
+              </TouchableOpacity>
+            </IconWrapper>
+          </View>
+
+          <View style={{ flexDirection: "row" }}>
+            <IconWrapper>
+              <TouchableOpacity
+                onPress={() => navigate("Inspiration")}
+                style={{ alignItems: "center" }}
+              >
+                <Image source={require("../../../assets/sun.png")} />
+                <IconName>Inspiration</IconName>
+              </TouchableOpacity>
+            </IconWrapper>
+            <IconWrapper>
+              <TouchableOpacity
+                onPress={() => navigate("Stores")}
+                style={{ alignItems: "center" }}
+              >
+                <Stores2 fillColor="#fff" innerCircle="#000" />
+                <IconName>Stores</IconName>
+              </TouchableOpacity>
+            </IconWrapper>
+          </View>
+        </TopSection>
+        <LowerSection>
+          <Text>My Account</Text>
+          <Line />
+          <TouchableOpacity onPress={() => navigate("CustomerSupport")}>
+            <Text>Customer Support</Text>
+          </TouchableOpacity>
+          <Line />
+          {this.state.isLoggeIn ? (
+            <TouchableOpacity onPress={this.handleLogout}>
+              <Text color="#F3B453">Logout</Text>
             </TouchableOpacity>
-          </IconWrapper>
-          <IconWrapper>
+          ) : (
             <TouchableOpacity
-              onPress={() => navigate("Stores")}
-              style={{ alignItems: "center" }}
+              onPress={() => this.props.navigation.navigate("Login")}
             >
-              <Stores2 fillColor="#fff" innerCircle="#000" />
-              <IconName>Stores</IconName>
+              <Text color="#F3B453">Login</Text>
             </TouchableOpacity>
-          </IconWrapper>
-        </View>
-      </TopSection>
-      <LowerSection>
-        <Text>My Account</Text>
-        <Line />
-        <TouchableOpacity onPress={() => navigate("CustomerSupport")}>
-          <Text>Customer Support</Text>
-        </TouchableOpacity>
-        <Line />
-        <Text color="#F3B453">Logout</Text>
-        <Line />
-        <SocialMediaWrapper>
-          <SubText>follow us</SubText>
-          <SocialMedia>
-            <Icon source={require("../../../assets/facebook.png")} />
-            <Icon source={require("../../../assets/twitter.png")} />
-            <Icon source={require("../../../assets/pinterest.png")} />
-          </SocialMedia>
-        </SocialMediaWrapper>
-      </LowerSection>
-    </Container>
-  );
-};
+          )}
+          <Line />
+          <SocialMediaWrapper>
+            <SubText>follow us</SubText>
+            <SocialMedia>
+              <Icon source={require("../../../assets/facebook.png")} />
+              <Icon source={require("../../../assets/twitter.png")} />
+              <Icon source={require("../../../assets/pinterest.png")} />
+            </SocialMedia>
+          </SocialMediaWrapper>
+        </LowerSection>
+      </Container>
+    );
+  }
+}
 
 export default CustomAppDrawer;
